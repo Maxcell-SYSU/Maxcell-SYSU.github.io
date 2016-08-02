3570ff33be22185dac689e6c694046cdb946b6b4
Alcanderian <alcanderian@gmail.com>
2016-08-03 02:10:27 +0800

    fixed-respon-2 demo-0.8

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,19 +27,60 @@ $(document).ready(function () {
     $('body').responsive({
         extraSmall: function () {
             $('.nav-controls').removeClass('pull-right');
+            if (document.getElementById("page1").offsetHeight <= 320 &&
+                document.getElementById("page1").offsetWidth <= 240) {
+                document.getElementById("viewport").content =
+                    "width=240, height=320";
+            }
+            if (document.getElementById("page1").offsetWidth <= 240) {
+                document.getElementById("viewport").content =
+                    "width=240, height=" + document.getElementById("page1").offsetHeight;
+            }
+            if (document.getElementById("page1").offsetHeight <= 320) {
+                document.getElementById("viewport").content =
+                    "height=320, width=" + document.getElementById("page1").offsetWidth;
+            }
         },
         small: function () {
             $('.home').hide();
+            if (document.getElementById("page1").offsetWidth <= 240) {
+                document.getElementById("viewport").content =
+                    "width=240, height=" + document.getElementById("page1").offsetHeight;
+            }
+            if (document.getElementById("page1").offsetHeight <= 320) {
+                document.getElementById("viewport").content =
+                    "height=320, width=" + document.getElementById("page1").offsetWidth;
+            }
         },
         medium: function () {
             $('.home').show();
+            if (document.getElementById("page1").offsetWidth <= 240) {
+                document.getElementById("viewport").content =
+                    "width=240, height=" + document.getElementById("page1").offsetHeight;
+            }
+            if (document.getElementById("page1").offsetHeight <= 320) {
+                document.getElementById("viewport").content =
+                    "height=320, width=" + document.getElementById("page1").offsetWidth;
+            }
         },
         large: function () {
-            if(document.getElementById("page1").offsetWidth >= 1920) {
+            if (document.getElementById("page1").offsetWidth <= 240) {
+                document.getElementById("viewport").content =
+                    "width=240, height=" + document.getElementById("page1").offsetHeight;
+            }
+            if (document.getElementById("page1").offsetHeight <= 320) {
+                document.getElementById("viewport").content =
+                    "height=320, width=" + document.getElementById("page1").offsetWidth;
+            }
+            if (document.getElementById("page1").offsetWidth >= 1920 &&
+                document.getElementById("page1").offsetHeight >= 1080) {
+                document.getElementById("viewport").content = "height=1080, width=1920";
+            }
+            if (document.getElementById("page1").offsetWidth >= 1920) {
                 document.getElementById("viewport").content =
                     "width=1920, height=" + document.getElementById("page1").offsetHeight;
             }
-            if(document.getElementById("page1").offsetHeight >= 1080) {
+            if (document.getElementById("page1").offsetHeight >= 1080) {
                 document.getElementById("viewport").content =
                     "height=1080, width=" + document.getElementById("page1").offsetWidth;
             }
