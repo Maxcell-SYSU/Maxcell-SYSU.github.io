b86ee039e894c08a04e547884ee850fed82da32d
Alcanderian <alcanderian@gmail.com>
2016-08-02 18:56:31 +0800

    demo-0.3

--- index.js
+++ index.js
@@ -3,68 +3,19 @@
  */
 
 document.getElementById("brand").onclick = function () {
-    var now_scroll = document.body.scrollTop;
-    var px_pre_move = now_scroll / 20;
-    var funScroll = function () {
-        var top = document.body.scrollTop;
-        if (top > 0) {
-            top -= px_pre_move;
-            if (top <= 0) top = 0;
-        } else return;
-        document.body.scrollTop = top;
-        requestAnimationFrame(funScroll);
-    };
-    if (now_scroll) funScroll();
+    $("html,body").animate({scrollTop:$("#page1").offset().top},500);
 };
 
 document.getElementById("home").onclick = function () {
-    var now_scroll = document.body.scrollTop;
-    var px_pre_move = now_scroll / 20;
-    var funScroll = function () {
-        var top = document.body.scrollTop;
-        if (top > 0) {
-            top -= px_pre_move;
-            if (top <= 0) top = 0;
-        } else return;
-        document.body.scrollTop = top;
-        requestAnimationFrame(funScroll);
-    };
-    if (now_scroll) funScroll();
+    $("html,body").animate({scrollTop:$("#page1").offset().top},500);
 };
 
 document.getElementById("about").onclick = function () {
-    if (document.body.scrollTop === 0) document.body.scrollTop = 1;
-    var desire_scroll = document.getElementById("page1").offsetHeight;
-    if (document.body.scrollTop >= desire_scroll * 2) document.body.scrollTop = document.body.scrollTop - 5;
-    var now_scroll = document.body.scrollTop;
-    var px_pre_move = (now_scroll - desire_scroll) / 20;
-    var funScroll = function () {
-        var top = document.body.scrollTop;
-        if (Math.abs(top - desire_scroll) > 1) {
-            top -= px_pre_move;
-            if (Math.abs(top - desire_scroll) < 5) top = desire_scroll;
-        } else return;
-        document.body.scrollTop = top;
-        requestAnimationFrame(funScroll);
-    };
-    if (now_scroll) funScroll();
+    $("html,body").animate({scrollTop:$("#page2").offset().top},500);
 };
 
 document.getElementById("contact").onclick = function () {
-    if (document.body.scrollTop === 0) document.body.scrollTop = 1;
-    var now_scroll = document.body.scrollTop;
-    var desire_scroll = document.getElementById("page1").offsetHeight * 2;
-    var px_pre_move = (desire_scroll - now_scroll) / 20;
-    var funScroll = function () {
-        var top = document.body.scrollTop;
-        if (top < desire_scroll) {
-            top += px_pre_move;
-            if (top >= desire_scroll) top = desire_scroll;
-        } else return;
-        document.body.scrollTop = top;
-        requestAnimationFrame(funScroll);
-    };
-    if (now_scroll) funScroll();
+    $("html,body").animate({scrollTop:$("#page3").offset().top},500);
 };
 
 document.getElementById("tobbs").onclick = function () {
